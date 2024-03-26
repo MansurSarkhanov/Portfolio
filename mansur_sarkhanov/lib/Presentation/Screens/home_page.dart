b89fd0289f846fdb6810de3de11dbd69d6d00192
MansurSarkhanov <sarxanov.mansur@gmail.com>
2024-03-26 16:02:24 +0400

    test

---
+++ mansur_sarkhanov/lib/Presentation/Screens/home_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+import '../../Utility/responsive.dart';
+import '../Components/dashboard_widget.dart';
+import '../Components/side_menu_widget.dart';
+import '../Components/summary_widget.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final isDesktop = Responsive.isDesktop(context);
+
+    return Scaffold(
+      drawer: !isDesktop
+          ? const SizedBox(
+              width: 250,
+              child: SideMenuWidget(),
+            )
+          : null,
+      endDrawer: Responsive.isMobile(context)
+          ? SizedBox(
+              width: MediaQuery.of(context).size.width * 0.8,
+              child: const SummaryWidget(),
+            )
+          : null,
+      body: SafeArea(
+        child: Row(
+          children: [
+            if (isDesktop)
+              const Expanded(
+                flex: 2,
+                child: SizedBox(
+                  child: SideMenuWidget(),
+                ),
+              ),
+            const Expanded(
+              flex: 7,
+              child: DashboardWidget(),
+            ),
+            if (isDesktop)
+              const Expanded(
+                flex: 3,
+                child: SummaryWidget(),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ mansur_sarkhanov/lib/app_router.dart
@@ -0,0 +1,10 @@
+import 'package:go_router/go_router.dart';
+
+import 'Presentation/Screens/home_page.dart';
+
+final router = GoRouter(routes: [
+  GoRoute(
+    path: "/",
+    builder: (context, state) => const HomePage(),
+  ),
+]);
